cbd18c132969a47f0b1c00e0a3eb2c752d00d4d1
Eric Lo <lxneng@gmail.com>
2012-06-12 16:21:40 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-version = '0.4.1'
+version = '0.4.2'
 setup(name="xpinyin",
       version=version,
       description="translate chinese hanzi to pinyin by python",
