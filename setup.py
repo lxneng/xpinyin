42987be96818759c6f535d57962b62b1988c1f04
Eric Lo <lxneng@gmail.com>
2012-08-30 10:49:31 +0800

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="xpinyin",
-      version='0.4.4',
+      version='0.4.5',
       description="translate chinese hanzi to pinyin by python",
       author="Eric Lo",
       author_email="lxneng@gmail.com",
@@ -12,5 +12,4 @@ setup(name="xpinyin",
       packages=find_packages('src'),
       package_dir={'': 'src'},
       include_package_data=True,
-      license="MIT License",
-      zip_safe=True)
+      license="MIT License")
