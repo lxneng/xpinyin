960b1302d8f737482bf7c19376f670be0be4625e
Eric Lo <lxneng@gmail.com>
2013-06-16 08:16:01 +0800

    fixes #8

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+from setuptools import find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 setup(name="xpinyin",
-      version='0.4.7',
+      version='0.4.8',
       description="translate chinese hanzi to pinyin by python",
+      long_description=README + '\n\n' + CHANGES,
       author="Eric Lo",
       author_email="lxneng@gmail.com",
       url="https://github.com/lxneng/xpinyin",
--- src/xpinyin/__init__.py
+++ src/xpinyin/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os.path
 
@@ -13,7 +12,7 @@ class Pinyin(object):
         In [1]: from xpinyin import Pinyin
         In [2]: p = Pinyin()
         In [3]: p.get_pinyin(u"上海")
-        Out[3]: 'shanghai'
+        Out[3]: 'shang-hai'
         In [4]: p.get_initials(u"上")
         Out[4]: 'S'
     请输入utf8编码汉字
@@ -23,21 +22,28 @@ class Pinyin(object):
     data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'Mandarin.dat')
 
-    def __init__(self):
+    def __init__(self, data_path=data_path):
         self.dict = {}
-        for line in open(self.data_path):
+        for line in open(data_path):
             k, v = line.split('\t')
             self.dict[k] = v
 
-    def get_pinyin(self, chars=u'你好', splitter=''):
+    def get_pinyin(self, chars=u'你好', splitter=u'-'):
         result = []
+        flag = 1
         for char in chars:
             key = "%X" % ord(char)
             try:
                 result.append(self.dict[key].split(" ")[0].strip()[:-1]
                               .lower())
+                flag = 1
             except KeyError:
-                result.append(char)
+                if flag:
+                    result.append(char)
+                else:
+                    result[-1] += char
+                flag = 0
+
         return splitter.join(result)
 
     def get_initials(self, char=u'你'):
