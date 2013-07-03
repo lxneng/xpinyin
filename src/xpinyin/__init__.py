f215d927fbe27beb5f7a55b4db51b77558258474
gareth <g.github2@bemused>
2013-07-03 15:43:43 +0200

    show tone marks (code from http://stackoverflow.com/questions/8200349/convert-numbered-pinyin-to-pinyin-with-tone-marks)

--- src/xpinyin/__init__.py
+++ src/xpinyin/__init__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import os.path
+import re
 
+PinyinToneMark = {
+    0: u"aoeiuv\u00fc",
+    1: u"\u0101\u014d\u0113\u012b\u016b\u01d6\u01d6",
+    2: u"\u00e1\u00f3\u00e9\u00ed\u00fa\u01d8\u01d8",
+    3: u"\u01ce\u01d2\u011b\u01d0\u01d4\u01da\u01da",
+    4: u"\u00e0\u00f2\u00e8\u00ec\u00f9\u01dc\u01dc",
+}
 
 class Pinyin(object):
     """translate chinese hanzi to pinyin by python, inspired by flyerhzm’s
@@ -28,14 +36,53 @@ class Pinyin(object):
             k, v = line.split('\t')
             self.dict[k] = v
 
+    def decode_pinyin(self, s):
+      s = s.lower()
+      r = ""
+      t = ""
+#      import pdb;pdb.set_trace()
+      for c in s:
+          if c >= 'a' and c <= 'z':
+              t += c
+          elif c == ':':
+              assert t[-1] == 'u'
+              t = t[:-1] + "\u00fc"
+          else:
+              if c >= '0' and c <= '5':
+                  tone = int(c) % 5
+                  if tone != 0:
+                      m = re.search("[aoeiuv\u00fc]+", t)
+                      if m is None:
+                          t += c
+                      elif len(m.group(0)) == 1:
+                          t = t[:m.start(0)] + PinyinToneMark[tone][PinyinToneMark[0].index(m.group(0))] + t[m.end(0):]
+                      else:
+                          if 'a' in t:
+                              t = t.replace("a", PinyinToneMark[tone][0])
+                          elif 'o' in t:
+                              t = t.replace("o", PinyinToneMark[tone][1])
+                          elif 'e' in t:
+                              t = t.replace("e", PinyinToneMark[tone][2])
+                          elif t.endswith("ui"):
+                              t = t.replace("i", PinyinToneMark[tone][3])
+                          elif t.endswith("iu"):
+                              t = t.replace("u", PinyinToneMark[tone][4])
+                          else:
+                              t += "!"
+              r += t
+              t = ""
+      r += t
+      return r
+
+
     def get_pinyin(self, chars=u'你好', splitter=u'-'):
         result = []
         flag = 1
         for char in chars:
             key = "%X" % ord(char)
             try:
-                result.append(self.dict[key].split(" ")[0].strip()[:-1]
-                              .lower())
+                result.append(self.decode_pinyin(self.dict[key].split(" ")[0]
+                  .strip().lower())
                 flag = 1
             except KeyError:
                 if flag:
